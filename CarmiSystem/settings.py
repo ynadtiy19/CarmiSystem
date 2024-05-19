8a9c4e54c3d9c9955e94be4284903276b235189f
stringency <1422699629@qq.com>
2024-05-19 15:08:00 +0800

    20

--- CarmiSystem/settings.py
+++ CarmiSystem/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+# STATIC_URL = "static/"
 STATIC_ROOT = "/data/CarmiSystem_static/static"
 
 # Default primary key field type
